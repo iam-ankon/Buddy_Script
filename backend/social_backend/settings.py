d1e6193c6faab555bbdc4a0ddb4550a7e391c731
Ankon <iam_ankon@Ankons-MacBook-Air.local>
2026-06-15 12:57:42 +0600

    commit

--- backend/social_backend/settings.py
+++ backend/social_backend/settings.py
@@ -118,9 +118,9 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 
